64ebfa01b4e1ada8010da3f22b6e5d333dc56fb0
0tae <choiyt3465@naver.com>
2025-06-16 21:06:25 +0900

    fix : 불필요한 파일 제거

--- mcp-server/mcp-server.py
+++
@@ -1,5 +0,0 @@
-# TODO: MCP 서버 구현
-# Google
-# Youtube
-# MySQL[강의계획서, 수강신청통계]
-# VectorDB-ChromaDB[교수 인터뷰]
--- models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Question(BaseModel):
-    # TODO: 질문과 학과 입력 등의 추가적인 정보 기입
-    instruction: str
-    user_id: Optional[str] = None
-
-class Answer(BaseModel):
-    text: str
