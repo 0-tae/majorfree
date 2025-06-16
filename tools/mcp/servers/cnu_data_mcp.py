c024bf7673e23699f31f6f6ab8df1d7e3d532365
0tae <choiyt3465@naver.com>
2025-06-16 21:07:29 +0900

    feat : 학과 정보 DB query를 수행하는 MCP 서버 구현

---
+++ tools/mcp/servers/cnu_data_mcp.py
@@ -0,0 +1,26 @@
+from mcp.server.fastmcp import FastMCP
+from tools.agent.sql_agent import SQLAgent
+
+sql_agent = SQLAgent(allowed_tables=['강의계획서','수강신청정보','학과정보'])
+
+
+mcp = FastMCP(
+    name="CNU Data MCP",
+    description="대학교 학과 학부 전공 강의 추천 및 관련 정보 조회, 강의계획서 조회에 특화된 도구. 사용자가 자신의 학년, 학과, 관심사를 말하면 가장 적합한 강의를 찾아 MySQL 쿼리로 반환함.",
+    host='localhost',
+    port = 8001
+)
+
+@mcp.tool(
+    name="CNU Data MCP",
+    description="대학교 학과 학부 전공 강의 추천 및 관련 정보 조회, 강의계획서 조회에 특화된 도구. 사용자가 자신의 학년, 학과, 관심사를 말하면 가장 적합한 강의를 찾아 MySQL 쿼리로 반환함.",
+)
+def query_for_cnu_data(user_input: str) -> str:
+    answer = sql_agent.question(user_input)
+    
+    result = answer["output"]
+    
+    return result
+
+print(f"MCP Server({mcp.name}) is running...")
+mcp.run(transport="sse")
