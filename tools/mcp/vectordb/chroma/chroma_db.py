8229f23c1924a5f3b2273bf3f7f62b6c461959e8
0tae <choiyt3465@naver.com>
2025-07-08 19:33:38 +0900

    feat : chromaDB 클래스 추가

---
+++ tools/mcp/vectordb/chroma/chroma_db.py
@@ -0,0 +1,28 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+
+class ChromaDB:
+    def __init__(self, collection_name:str = "interview_data"):
+        self.__client = chromadb.PersistentClient(path="./vectordb/chroma/chroma")
+        self.__collection = self.__client.get_or_create_collection(name = collection_name)
+        self.__embedding_model = SentenceTransformer("snunlp/KR-SBERT-V40K-klueNLI-augSTS")
+
+    def __get_embeddings(self, input_str: str):
+        return self.__embedding_model.encode(input_str)
+
+    def query(self, input: str, filter:list = [], n_results:int = 10):
+        doc_embeddings = self.__get_embeddings(input)
+        
+        # filter가 빈 리스트가 아닐 때만 where 조건 적용
+        where_condition = {"학과명": {"$in": filter}} if filter else None
+        
+        query_result = self.__collection.query(
+                query_embeddings=doc_embeddings,
+                where=where_condition,
+                n_results=n_results
+        )
+        print("ids: ", query_result['ids'][0])
+        return query_result['ids'][0],query_result["documents"][0]
+
+
+db_instance = ChromaDB(collection_name="interview_data")
