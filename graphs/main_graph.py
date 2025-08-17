a3e3df550ad1d149e393e5a642448d589fb6395e
0tae <choiyt3465@naver.com>
2025-08-18 03:06:26 +0900

    feat : 실시간 스트림 run_astream 구현 - 미사용 노드 코드 제거

--- graphs/main_graph.py
+++ graphs/main_graph.py
@@ -8,11 +8,10 @@ from graphs.nodes.department_search import department_search
 from graphs.nodes.fast_forward_question import fast_forward_question
 from graphs.nodes.init import init
 from graphs.nodes.kocw_search import kocw_search
-from graphs.nodes.merge_messages import merge_messages
+from graphs.nodes.merge import merge_messages
 from graphs.nodes.route import route
 from graphs.nodes.web_search import web_search
 from graphs.nodes.youtube_search import youtube_search
-from graphs.nodes.none_action import none_action
 
 import uuid
 from utils import time_measurement
@@ -34,13 +33,6 @@ class AgentGraphApplication:
         self.workflow.add_node("agent_question", agent_question)
         self.workflow.add_node("fast_forward_question", fast_forward_question)
         
-        # node - search all
-        self.workflow.add_node("search_all_start", none_action)
-        self.workflow.add_node("search_all_youtube",youtube_search)
-        self.workflow.add_node("search_all_kocw",kocw_search)
-        self.workflow.add_node("search_all_web",web_search)
-        self.workflow.add_node("search_all_end", none_action)
-        
         # node - summarize & evaluate
         self.workflow.add_node("merge_messages", merge_messages)
         
@@ -58,7 +50,6 @@ class AgentGraphApplication:
                 "DEPARTMENT_SEARCH": "department_search",
                 "COMMON": "agent_question",
                 "FAST_FORWARD": "fast_forward_question",
-                "SEARCH_ALL": "search_all_start"
             }
         )
         
@@ -68,13 +59,7 @@ class AgentGraphApplication:
         self.workflow.add_edge("web_search", "merge_messages")
         self.workflow.add_edge("department_search", "merge_messages")
         self.workflow.add_edge("agent_question", "merge_messages")
-        
-         # edge - search all
-        self.workflow.add_edge("search_all_start", "search_all_youtube")
-        self.workflow.add_edge("search_all_youtube", "search_all_kocw")
-        self.workflow.add_edge("search_all_kocw", "search_all_web")
-        self.workflow.add_edge("search_all_web", "search_all_end")
-        self.workflow.add_edge("search_all_end", "merge_messages")
+    
         
         # edge - depth final
         self.workflow.add_edge("fast_forward_question", END)
@@ -92,6 +77,18 @@ class AgentGraphApplication:
                                 "search_type": search_type.upper(), 
                                 "optional_args": optional_args},
                                config={"configurable": {"thread_id": uuid.uuid4()}})
-        
+    
+    async def run_astream(self, question: str, existing_messages: List[Dict[str, Any]] = [], search_type: str = "COMMON", optional_args: Dict[Any, Any] = {}):
+        async for chunk in self.app.astream(
+            {
+                "instruction": question,
+                "messages": existing_messages,
+                "search_type": search_type.upper(), 
+                "optional_args": optional_args
+            },
+            config={"configurable": {"thread_id": uuid.uuid4()}},
+            stream_mode="messages"
+        ):
+            yield chunk  # chunk를 필요에 따라 join/누적/즉시 반환
         
 graph_agent_instance = AgentGraphApplication()
